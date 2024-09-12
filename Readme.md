20dc6d73abe87a212dbd69ca2273444ad1bfad18
iamhvsharma <itshvsharma@gmail.com>
2024-09-13 00:03:52 +0530

    MongoDB connected

--- Readme.md
+++ Readme.md
@@ -1,3 +1,14 @@
 # Backend Project
 
 A YT Functionality Clone Project
+
+Packages We need:
+
+1. mongoose
+2. express
+3. dotenv
+
+
+Dev Dependency:-
+1. Nodemon
+2. Prettier
---
+++ src/index.js
@@ -0,0 +1,51 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+dotenv.config({
+    path: "./env"
+})
+
+
+import connectDB from "./db/index.js";
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+/*
+import mongoose, { mongo } from "mongoose";
+import { DB_NAME } from "./constants";
+
+import express from "express";
+const app = express();
+
+// IIFE Function- Immediately invoking  function expression
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    app.on("error", () => {
+        console.log("ERR: ", error);
+        throw error;
+        
+    })
+
+    app.listen(process.env.PORT, () => {
+        console.log(`Listening at port ${process.env.PORT}`);
+        
+    })
+
+
+  } catch (error) {
+    console.log("ERROR: ", error);
+    throw error;
+  }
+})();
+
+*/
