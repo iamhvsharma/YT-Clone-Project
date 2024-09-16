135cee9fd819d3e0b6aca86bb64d497f10e39887
iamhvsharma <itshvsharma@gmail.com>
2024-09-16 22:59:35 +0530

    DB Connection

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 // require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config({
-    path: "./env"
-})
-
+  path: "./env",
+});
 
 import connectDB from "./db/index.js";
 
-
 connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERR: ", error);
+      throw error;
+    });
 
-
-
-
-
-
-
-
-
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Sever is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MongoDB Connection Failed, ERROR: ${err}`);
+  });
 
 /*
 import mongoose, { mongo } from "mongoose";
